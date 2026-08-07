8d5391b83bf07601e517f25f8d737ce5ff9ed9df
Nathan <nlesueur@gmail.com>
2026-08-07 15:05:01 +0000

    replaced requests retry with tenacity

--- bucket_report.py
+++ bucket_report.py
@@ -19,12 +19,14 @@ import datetime
 from kubernetes import client, config
 import argparse
 import sched
+from sqlalchemy import create_engine
+from tenacity import retry, wait_exponential, stop_after_attempt, retry_if_exception_type
 
 
+null_payload = [{'hostname': 'null.us-sea-9.linodeobjects.com', 'label': 'null', 'created': '1970-01-01T06:07:52', 'region': 'us-sea', 'cluster': 'us-sea-1', 'size': 0, 'objects': 0, 'endpoint_type': 'E3', 's3_endpoint': 'us-sea-9.linodeobjects.com'},]
 apiversion = "v4"
 
 today_date = datetime.date.today()
-#today_date = datetime.datetime.strptime("2026-07-02", "%Y-%m-%d").date()
 
 parser = argparse.ArgumentParser(description="Script with flags")
 parser.add_argument("-r", "--report", action="store_true", help="Run usage report")
@@ -32,6 +34,18 @@ args = parser.parse_args()
 run_report = args.report
 if not run_report:
     run_report = os.environ.get("RUN_REPORT", False)
+
+def _db_for_pd_conn():
+    db_user = os.environ["REPORT_DB_USER"]
+    db_pass = os.environ["REPORT_DB_PASS"]
+    db_host = os.environ["REPORT_DB_HOST"]
+    engine = create_engine(
+        "postgresql+psycopg2://{0}:{1}@{2}/defaultdb?sslmode=require".format(
+            db_user, db_pass, db_host
+        )
+    )
+
+    return engine
 
 def _db_conn():
     db_user = os.environ["REPORT_DB_USER"]
@@ -45,6 +59,17 @@ def _db_conn():
 
     return conn
 
+
+def handle_fallback(retry_state):
+    print("🚨 CRITICAL: Tenacity retry limit reached!")
+    print(f"Exception that caused the final failure: {retry_state.outcome.exception()}")
+    return bucket_detail(null_payload)
+
+def region_handle_fallback(retry_state):
+    print("🚨 CRITICAL: Tenacity retry limit reached!")
+    print(f"Exception that caused the final failure: {retry_state.outcome.exception()}")
+    return {"status": "failed", "fallback_applied": True}
+
 def _db_init():
     conn = _db_conn()
     cursor = conn.cursor()
@@ -57,26 +82,27 @@ def _db_region_init():
     conn = _db_conn()
     cursor = conn.cursor()
     cursor.execute(
-        "CREATE TABLE IF NOT EXISTS regions (region varchar(50), vos360_buckets SMALLINT, msl_buckets SMALLINT, sandbox_buckets SMALLINT, vos360_counts SMALLINT, msl_counts SMALLINT, sandbox_counts SMALLINT, last_checked_date TIMESTAMPTZ, last_error_site varchar(6), error_code int2, PRIMARY KEY (region))"
+        "CREATE TABLE IF NOT EXISTS regions (region varchar(50), vos360_buckets SMALLINT, msl_buckets SMALLINT, sandbox_buckets SMALLINT, vos360_counts SMALLINT, msl_counts SMALLINT, sandbox_counts SMALLINT, last_checked_date TIMESTAMPTZ, last_error_site varchar(100), error_code int2, PRIMARY KEY (region))"
     )
     return cursor, conn
 
+@retry(
+    stop=stop_after_attempt(5), 
+    wait=wait_exponential(multiplier=5, min=2, max=60), 
+    retry=retry_if_exception_type(requests.RequestException), 
+    retry_error_callback=region_handle_fallback 
+)
 def get_regions(token):
     headers = {"accept": "application/json", "authorization": "Bearer {}".format(token)}
     url = "https://api.linode.com/{0}/object-storage/endpoints".format(apiversion)
 
-    s = requests.Session()
-
-    retries = Retry(total=5, backoff_factor=0.5, status_forcelist=[500, 502, 503, 504])
-
-    s.mount("https://", HTTPAdapter(max_retries=retries))
-
-    response = s.get(url, headers=headers)
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
     data = response.json()
     cursor, conn = _db_region_init()
     #print(data)
+    print(f"Result: {response}")
     if response.status_code in [400, 422]:
-        response.raise_for_status()
         print("Terminating: {0}".format(response.status_code))
         exit()
 
@@ -102,7 +128,6 @@ def restart_deployment(name, namespace="metrics-system"):
     config.load_incluster_config()
     apps_v1 = client.AppsV1Api()
 
-    # now = datetime.datetime.utcnow().isoformat() + "Z"
     now = datetime.datetime.now(datetime.timezone.utc).isoformat() + "Z"
 
     body = {
@@ -124,12 +149,15 @@ def get_dc(k):
         "fremont": "Fremont, CA (us-west)",
         "us-west": "Fremont, CA (us-west)",
         "atlanta": "Atlanta, GA (us-southeast)",
+        "us-southeast": "Atlanta, GA (us-southeast)",
         "newark": "Newark, NJ (us-east)",
+        "us-east": "Newark, NJ (us-east)",
         "london": "London, UK (eu-west)",
         "eu-west": "London, UK (eu-west)",
         "ap-northeast-1a": "Tokyo, JP (ap-northeast-1a)",
         "singapore": "Singapore, SG (ap-south)",
         "frankfurt": "Frankfurt, DE (eu-central)",
+        "eu-central": "Frankfurt, DE (eu-central)",
         "shinagawa1": "Tokyo 2, JP (ap-northeast)",
         "us-east-1b": "Cedar Knolls, NJ, USA (us-east-1b)",
         "philadelphia": "Philadelphia, PA, USA (philadelphia)",
@@ -139,18 +167,27 @@ def get_dc(k):
         "iad3": "Washington, DC (us-iad)",
         "us-iad": "Washington, DC (us-iad)",
         "ord2": "Chicago, IL (us-ord)",
+        "us-ord": "Chicago, IL (us-ord)",
         "us-ord-1": "Chicago, IL (us-ord-1)",
         "us-ord-10": "Chicago, IL (us-ord-10)",  # changed
+        "fr-par": "Paris, FR (fr-par)",
         "sea1": "Seattle, WA (us-sea)",
         "us-sea-9": "Seattle, WA (us-sea)",
         "us-sea-1": "Seattle, WA (us-sea)",
+        "us-sea": "Seattle, WA (us-sea)",
         "gru1": "Sao Paulo, BR (br-gru)",
+        "br-gru": "Sao Paulo, BR (br-gru)",
         "ams2": "Amsterdam, NL (nl-ams)",
+        "nl-ams": "Amsterdam, NL (nl-ams)",
         "sto2": "Stockholm, SE (se-sto)",
         "mad2": "Madrid, ES (es-mad)",
         "maa1": "Chennai, IN (in-maa)",
+        "osa1": "Osaka, JP (jp-osa)",
         "mil1": "Milan, IT (it-mil)",
+        "us-mia": "Miami, FL (us-mia)",
+        "cgk1": "Jakarta, ID (id-cgk)",
         "lax3": "Los Angeles, CA (us-lax)",
+        "us-lax": "Los Angeles, CA (us-lax)",
         "us-lax-1": "Los Angeles, CA (us-lax-1)",
         "us-hou": "Houston, TX (us-hou)",
         "nz-akl-1": "Auckland, NZ (nz-akl-1)",
@@ -168,47 +205,27 @@ def get_dc(k):
         "gb-lon-4": "London 4, UK (gb-lon-4)",
         "gb-lon-1": "London, UK (gb-lon-1)",  # changed
         "gb-lon-2": "London 2, UK (gb-lon-2)",  # changed
+        "gb-lon": "London, UK (gb-lon)",  # changed
         "mel1": "Melbourne, AU (au-mel)",
+        "au-mel": "Melbourne, AU (au-mel)",
         "au-mel-1": "Melbourne, AU (au-mel-1)",
         "bom1": "Mumbai 2, IN (in-bom-2)",
         "in-bom-1": "Mumbai, IN (in-bom-1)",  # changed
+        "in-bom-2": "Mumbai 2, IN (in-bom-2)",
         "de-fra-1": "Frankfurt, DE (de-fra-1)",  # changed
+        "de-fra-2": "Frankfurt 2, DE (de-fra-2)",
+        "sg-sin-2": "Singapore 2, SG (sg-sin-2)",
         "sg-sin-1": "Singapore 2, SG (sg-sin-1)",
         "sg-sin": "Singapore 2, SG (sg-sin)",
         "tyo2": "Tokyo 3, JP (jp-tyo-3)",
         "jp-tyo-1": "Tokyo, JP (jp-tyo-1)",
         "jp-tyo": "Tokyo, JP (jp-tyo)",
         "jp-tyo-2": "Tokyo 2, JP (jp-tyo-2)",
+        "jp-tyo-3": "Tokyo 3, JP (jp-tyo-3)",
         "de-ber": "Berlin, DE (de-ber)",
         "de-ber-1": "Berlin, DE (de-ber-1)",
-        "no-osl": "Oslo, NO (no-osl)",
-        "us-sea": "Seattle, WA (us-sea)",
-        "us-southeast": "Atlanta, GA (us-southeast)",
-        "id-cgk": "Jakarta, ID (id-cgk)",
-        "jp-osa": "Osaka, JP (jp-osa)",
-        "in-maa": "Chennai, IN (in-maa)",
-        "us-mia": "Miami, FL (us-mia)",
-        "fr-par": "Paris, FR (fr-par)",
-        "fr-par-2": "Paris 2, FR (fr-par-2)",
-        "ap-south": "Singapore, SG (ap-south)",
-        "in-bom-2": "Mumbai 2, IN (in-bom-2)",
-        "de-fra-2": "Frankfurt 2, DE (de-fra-2)",
-        "us-iad": "Washington, DC (us-iad)",
-        "us-iad-2": "Washington 2, DC (us-iad-2)",
-        "us-ord": "Chicago, IL (us-ord)",
-        "br-gru": "Sao Paulo, BR (br-gru)",
-        "es-mad": "Madrid, ES (es-mad)",
         "no-osl-1": "Oslo, NO (no-osl-1)",
-        "us-lax": "Los Angeles, CA (us-lax)",
-        "us-east": "Newark, NJ (us-east)",
-        "jp-tyo-3": "Tokyo 3, JP (jp-tyo-3)",
-        "eu-central": "Frankfurt, DE (eu-central)",
-        "sg-sin-2": "Singapore 2, SG (sg-sin-2)",
-        "se-sto": "Stockholm, SE (se-sto)",
-        "it-mil": "Milan, IT (it-mil)",
-        "gb-lon": "London, UK (gb-lon)",
-        "au-mel": "Melbourne, AU (au-mel)",
-        "nl-ams": "Amsterdam, NL (nl-ams)",
+        "no-osl": "Oslo, NO (no-osl)",
     }
     try:
         response = dc[k]
@@ -253,7 +270,8 @@ def upload_file(file_name, bucket, object_name=None):
 
 def get_single_region(env, conn):
     sql_query = "SELECT * FROM regions;"
-    region_df = pd.read_sql_query(sql_query, conn)
+    pd_conn = _db_for_pd_conn()
+    region_df = pd.read_sql_query(sql_query, pd_conn)
     env_buckets = "{0}_buckets".format(env)
     env_counts = "{0}_counts".format(env)
     df_sorted = region_df.sort_values(by=env_counts)
@@ -272,6 +290,12 @@ def get_single_region(env, conn):
         if row[env_counts] == None or math.isnan(row[env_counts]):
             row[env_counts] = 0
 
+@retry(
+    stop=stop_after_attempt(5), 
+    wait=wait_exponential(multiplier=5, min=2, max=60), 
+    retry=retry_if_exception_type(requests.RequestException), 
+    retry_error_callback=handle_fallback 
+)
 def get_bucket_detail(token, env, cursor, conn, region, row):
     dt_utc = datetime.datetime.now(timezone.utc)
     env_buckets = "{0}_buckets".format(env)
@@ -281,21 +305,19 @@ def get_bucket_detail(token, env, cursor, conn, region, row):
     headers = {"accept": "application/json", "authorization": "Bearer {}".format(token)}
     url = "https://api.linode.com/{0}/object-storage/buckets/{1}".format(apiversion, region)
 
-    s = requests.Session()
 
-    retries = Retry(total=5, backoff_factor=0.5, status_forcelist=[500, 502, 503, 504])
-    s.mount("https://", HTTPAdapter(max_retries=retries))
-    response = s.get(url, headers=headers)
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
     print("{}".format(response.headers))
     data = response.json()
-    print(data["data"])
 
     if response.status_code in [400, 422]:
-        response.raise_for_status()
+        #response.raise_for_status()
         print("Terminating: {0}".format(response.status_code))
         exit()
 
     elif response.status_code == 200:
+        print(data["data"])
         sql = "INSERT INTO regions (region, {0}_buckets, {0}_counts, last_checked_date) VALUES (%s, %s, %s, %s) ON CONFLICT (region) DO UPDATE SET {0}_buckets = EXCLUDED.{0}_buckets, {0}_counts = EXCLUDED.{0}_counts, last_checked_date = EXCLUDED.last_checked_date;".format(env)
 
         if row[env_counts] == None or math.isnan(row[env_counts]):
@@ -311,6 +333,7 @@ def get_bucket_detail(token, env, cursor, conn, region, row):
         print("Pages {}".format(data["pages"]))
 
         if data["pages"] == 1:
+            print("Data {}".format(data["data"]))
             return bucket_detail(data["data"])
         else:
             page = 2
@@ -321,36 +344,37 @@ def get_bucket_detail(token, env, cursor, conn, region, row):
                         apiversion, region, page
                     )
                 )
-                try:
-                    response_p = s.get(url, headers=headers)
-                    print("{}".format(response_p.headers))
-                except requests.exceptions.RequestException as e:
-                    print(f"The request ultimately failed: {e}")
-                # response_p = requests.get(url, headers=headers)
-                finally:
-                    datapage = response_p.json()
+                response_p = requests.get(url, headers=headers)
+                response_p.raise_for_status()
+                print("{}".format(response_p.headers))
+                datapage = response_p.json()
                 for x in datapage["data"]:
                     data["data"].append(x)
+                if response.status_code in [500, 501, 502, 503, 504]:
+                    sql = "INSERT INTO regions (region, last_error_site, error_code) VALUES (%s, %s, %s) ON CONFLICT (region) DO UPDATE SET last_error_site = EXCLUDED.last_error_site, error_code = EXCLUDED.error_code;"
+                    cursor.execute(
+                        sql,
+                        (region, env, response.status_code),
+                    )
+                    conn.commit()
+
+                    print(f"The request ultimately failed: {e}")
                 page += 1
 
             return bucket_detail(data["data"])
-    elif response.status_code == 504:
+    elif response.status_code in [500, 501, 502, 503, 504]:
         sql = "INSERT INTO regions (region, last_error_site, error_code) VALUES (%s, %s, %s) ON CONFLICT (region) DO UPDATE SET last_error_site = EXCLUDED.last_error_site, error_code = EXCLUDED.error_code;"
         cursor.execute(
             sql,
             (region, env, response.status_code),
         )
         conn.commit()
-        return
 
 
 def bucket_detail(data):
     df = pd.DataFrame(data)
-    # df.style.hide(axis='index')
 
     return df
-
-
 
 def _do_query(cursor, conn, env, token, region, row):
     resp = get_bucket_detail(token, env, cursor, conn, region, row)
@@ -362,8 +386,6 @@ def _do_query(cursor, conn, env, token, region, row):
         size = line.size
         print("{},{},{}".format(label, region, size))
         linec += 1
-        # l = line.decode("utf-8")
-        # size = float(size)
         cursor.execute(
             "INSERT INTO buckets (bucket_name, contract_id, datacenter, size, checked_date) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (checked_date, bucket_name, datacenter) DO UPDATE SET contract_id = EXCLUDED.contract_id, size = EXCLUDED.size;",
             (label, get_cid(env)[0], get_dc(region), size, today_date),
@@ -372,7 +394,6 @@ def _do_query(cursor, conn, env, token, region, row):
 
 
 report_cycle = 60
-
 
 def reporting_task(scheduler):
     envs = [
@@ -411,11 +432,12 @@ if not run_report:
 if run_report:
     print("Running report...")
     cursor, conn = _db_init()
+    pd_conn = _db_for_pd_conn()
     sql_query = "SELECT * FROM buckets WHERE checked_date = CURRENT_DATE - INTERVAL '1 day';"
-    #sql_query = "SELECT * FROM buckets WHERE checked_date = CURRENT_DATE;"
-    df = pd.read_sql_query(sql_query, conn)
+    df = pd.read_sql_query(sql_query, pd_conn)
     df = df.drop(columns='checked_date')
     df.columns = ["Bucket Name", "Contract ID", "Datacenter", "Size in Bytes"]
+    df = df[df["Bucket Name"] != "null"]
     print(df.to_string())
 
     os.makedirs("/var/report", exist_ok=True)
